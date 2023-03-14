a4de5aed63dd33f280d4bbadb8af69bdcaaf7bf8
JerryKwan <ggxx936099147@mail.ustc.edu.cn>
2023-03-14 17:02:23 +0800

    finish simple gpu accelerate without dl

---
+++ src/gpu_no_dl/bitahub1.sh
@@ -0,0 +1,7 @@
+# used in bitahub debug mode
+bash Miniconda3-latest-Linux-x86_64.sh # install under /root, and remember conda init
+source ~/.bashrc
+conda install -c conda-forge openmm # cudatoolkit will also be installed
+
+# https://stackoverflow.com/questions/72540359/glibcxx-3-4-30-not-found-for-librosa-in-conda-virtual-environment-after-tryin
+conda install -c conda-forge gcc=12.1.0
