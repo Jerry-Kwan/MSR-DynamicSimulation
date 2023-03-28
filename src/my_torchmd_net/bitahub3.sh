cad9c431bab3a446c5a0df21b2465ea8c744756e
JerryKwan <ggxx936099147@mail.ustc.edu.cn>
2023-03-28 22:49:01 +0800

    modify env setup scripts for torchmd and torchmd-net

---
+++ src/my_torchmd_net/bitahub3.sh
@@ -0,0 +1,10 @@
+# used in bitahub debug mode
+bash Mambaforge-Linux-x86_64.sh # install under /root, and remember init
+source ~/.bashrc
+
+# environment.yml is under my_torchmd_net
+mamba env create -f environment.yml
+mamba activate torchmd-net
+
+# Then follow the instructions in the TorchMD-NET repo.
+# That is, cloning the repo, pip install -e . and follow the command in examples/ file folder.
--- src/torchmd/bitahub2.sh
+++ src/torchmd/bitahub2.sh
@@ -2,20 +2,13 @@
 bash Mambaforge-Linux-x86_64.sh # install under /root, and remember init
 source ~/.bashrc
 
-# here, create a new mamba env with python=3.9 and activate it
+# here, create a new mamba env with **python=3.9** and activate it
 # ...
 
 # download pytorch
-pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
+mamba install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
 
 # download other packages for torchmd env
-mamba install moleculekit -c acellera -c conda-forge
 mamba install ipython
 pip install torchmd parmed
-
-########################################################
-# packages used for torchmd-net env
-mamba install -c conda-forge h5py matplotlib tqdm nnpops==0.2 pytorch_cluster==1.5.9 pytorch_geometric==2.0.3 pytorch_scatter==2.0.8 pytorch_sparse==0.6.10 pytorch-lightning==1.6.3 torchmetrics==0.8.2 rdkit
-
-# Then follow the instructions in the TorchMD-NET repo.
-# That is, cloning the repo, pip install -e . and follow the command in examples/ file folder.
+mamba install moleculekit -c acellera -c conda-forge
