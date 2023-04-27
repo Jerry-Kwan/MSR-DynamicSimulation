00b3417a8238d42448279d1959de895980f126d6
JerryKwan <ggxx936099147@mail.ustc.edu.cn>
2023-04-27 16:53:22 +0800

    refactor openmm with gpu

--- src/gpu_no_dl/bitahub1.sh
+++
@@ -1,7 +0,0 @@
-# used in bitahub debug mode with pytorch 1.11 and python 3.8
-bash Miniconda3-latest-Linux-x86_64.sh # install under /root, and remember conda init
-source ~/.bashrc
-conda install -c conda-forge openmm # cudatoolkit will also be installed
-
-# https://stackoverflow.com/questions/72540359/glibcxx-3-4-30-not-found-for-librosa-in-conda-virtual-environment-after-tryin
-conda install -c conda-forge gcc=12.1.0
