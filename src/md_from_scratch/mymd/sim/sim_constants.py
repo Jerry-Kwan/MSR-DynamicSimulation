7b820125c080e8f783cce4652dd25635daed01f9
JerryKwan <ggxx936099147@mail.ustc.edu.cn>
2023-05-06 21:19:47 +0800

    add part of simulation in md_from_scratch

---
+++ src/md_from_scratch/mymd/sim/sim_constants.py
@@ -0,0 +1,7 @@
+"""
+References:
+    1. https://github.com/torchmd/torchmd
+"""
+
+TIMEFACTOR = 48.88821
+BOLTZMANN = 0.001987191
--- src/md_from_scratch/mymd/sim/simulation.py
+++ src/md_from_scratch/mymd/sim/simulation.py
@@ -1,8 +1,47 @@
+import numpy as np
+import torch
+
+from .sim_constants import BOLTZMANN
+
+
 class Simulation(object):
-    """
-    TOBEDONE
-    Remember to implement device and precision (platformProperties) methods in torchmd.Parameters.
+    """This class controls the whole simulation procedure.
+
+    Imitating openmm.app.simulation.Simulation
+
+    Including:
+        1. TOBEDONE
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, mol, system, integrator, device, dtype):
+        self.mol = mol
+        self.system = system
+        self.integrator = integrator
+        self.device = device
+        self.dtype = dtype
+
+        self._build_simulation()
+
+    def _build_simulation(self):
+        self.system.set_device_and_dtype(self.device, self.dtype)
+
+        n = self.system.num_atoms
+        self.pos = torch.zeros(n, 3).type(self.dtype).to(self.device)
+        self.vel = torch.zeros(n, 3).type(self.dtype).to(self.device)
+        self.forces = torch.zeros(n, 3).type(self.dtype).to(self.device)
+
+    def set_positions(self, pos):
+        assert pos.shape == (self.system.num_atoms, 3, 1), f'Shape of pos is not {(self.system.num_atoms, 3, 1)}'
+
+        pos = np.squeeze(pos, 2)
+        self.pos[:] = torch.tensor(pos, dtype=self.dtype, device=self.device)
+
+    def set_velocities_to_temperature(self, T):
+        """
+        Set the velocities of all particles in the System to random values chosen from a
+        Maxwell-Boltzmann distribution at a given temperature.
+        """
+        std_normal_dist = torch.randn((self.system.num_atoms, 3)).type(self.dtype)
+        mb_dist = torch.sqrt(T * BOLTZMANN / self.system.masses) * std_normal_dist
+
+        self.vel[:] = mb_dist.type(self.dtype).to(self.device)
